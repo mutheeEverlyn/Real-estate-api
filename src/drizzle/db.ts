8a337552ba784872cc2b74f4ce269e47e583213a
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-11-20 21:45:47 +0300

    new file

---
+++ src/drizzle/db.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+import * as schema from "./schema"
+import Stripe from 'stripe';
+export const client = new Client({
+    connectionString: process.env.DATABASE_URL as string,   //get the database url from the environment
+})
+
+const main = async () => {
+    await client.connect();  //connect to the database
+}
+main();
+
+
+const db = drizzle(client, { schema, logger: true })  //create a drizzle instance
+
+export default db; 
+export const stripe=new Stripe (process.env.STRIPE_SECRET_API_KEY as string,{
+    apiVersion:'2024-06-20',
+    typescript:true
+});
